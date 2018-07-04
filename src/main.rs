7d8895c3cb578c1fbca4345f7875fe8af60e1840
johannesvollmer <johannes.boenki@t-online.de>
2018-07-04 14:01:17 +0200

    Added example on how to use the API

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,18 @@ pub mod referenced;
 
 
 fn main() {
+    let parsed = ::flat::FlatObject::parse(r#"
+
+        ok_text: 'Ok'
+
+        Button: {
+            visible: 'true'
+            text: 'Click Here'
+        }
+
+        ok_button: Button { text: ok_text }
+
+    "#);
+
+    println!("parsed: \n{:#?}", parsed.expect("Parsing Error"));
 }
